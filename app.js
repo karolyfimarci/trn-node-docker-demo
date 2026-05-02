0c71d3f4a6b4c2a325ab6c28b9dbbc0625a6786c
karolyfimarci <89314231+karolyfimarci@users.noreply.github.com>
2026-05-02 19:58:47 +0200

    Hiba javítása a CI folyamat sikeréhez

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(path.dirname(new URL(import.meta.url).pathname)
 /* Kezdőlap */
 app.get('/', (req, res) => {
   const NodeVersion = process.versions;
-  res.render('index', { data: { nodeVersion: NodeVersion.node, title: 'Docker alkalmazás példa', host: hostname }, uzenetLista });
+  res.render('index', { data: { nodeVersion: NodeVersion.node, title: 'Azure Docker 2026', host: hostname }, uzenetLista });
 });
 
 /* Mentés */
